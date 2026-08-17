14b598e66139db609f767ecde460f0bcd6a9fb79
MahvOS <mahvinaflahmulyana@gmail.com>
2026-08-17 20:13:38 +0700

    second commit looolll

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://placeholder.supabase.co";
+const supabaseKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
+  "placeholder-publishable-key";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
